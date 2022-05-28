f3f7aed7e9c0981afff294faa3c44953160d8959
nitin002318 <106405329+nitin002318@users.noreply.github.com>
2022-05-28 22:45:36 +0530

    Create Create alphabetcheck.py

---
+++ Create alphabetcheck.py
@@ -0,0 +1,7 @@
+apha=input("Enter only single character  ")
+if apha=="a" or apha=="e" or apha=="i" or apha=="o" or apha=="u" or apha=="A" or apha=="E" or apha=="I" or apha=="O" or apha=="U":
+    print("Yes,it is a vowel")
+elif ord(apha)>=65 and ord(apha)<=90 or ord(apha)>=97 and ord(apha)<=122:
+    print("yes, it is a consonant")
+else:
+    print("Not, valid")
