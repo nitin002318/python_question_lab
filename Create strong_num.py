592cb9180177f789ccb0dcf4aae63e56e1561b84
nitin002318 <106405329+nitin002318@users.noreply.github.com>
2022-05-28 22:51:23 +0530

    Create Create strong_num.py

---
+++ Create strong_num.py
@@ -0,0 +1,14 @@
+num=int(input("Enter the number"))
+temp=num
+sum=0
+while num!=0:
+    d=num%10
+    fact=1
+    for i in range(1,d+1):
+        fact*=i
+    sum+=fact
+    num//=10
+if sum==temp:
+    print("Yes, it is a strong number")
+else:
+    print("No, it is not a strong number")
