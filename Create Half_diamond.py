dc2132e7f97df664de509964af23a14747623fea
nitin002318 <106405329+nitin002318@users.noreply.github.com>
2022-05-28 22:37:15 +0530

    Create Create Half_diamond.py

---
+++ Create Half_diamond.py
@@ -0,0 +1,13 @@
+n=int(input("Enter a number :"))
+for i in range(1,n+1):
+    for j in range(i,n):
+        print(" ",end='')
+    for k in range(1,i+1):
+        print("*",end='')
+    print() 
+for i in range(n-1,0,-1):
+    for j in range(i,n):
+        print(" ",end='')
+    for k in range(1,i+1):
+        print("*",end='')
+    print() 
