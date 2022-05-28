5b3f9ad3b7cf05c99876ab2c96ecb0e1976bbf79
nitin002318 <106405329+nitin002318@users.noreply.github.com>
2022-05-28 22:33:08 +0530

    Create Create arthmetic.py

---
+++ Create arthmetic.py
@@ -0,0 +1,19 @@
+n1=int(input("Enter the first number"))
+n2=int(input("Enter the second number"))
+op=input("Enter the operator ")
+
+if op=="+":
+    res=n1+n2
+    print(res)
+elif op=="-":
+    res=n1-n2
+    print(res)
+elif op=="*":
+    res=n1*n2
+    print(res)
+elif op=="/":
+    res=n1/n2
+    print(res)
+elif op=="%":
+    res=n1%n2
+    print(res)
